573dcda46bbd15f200dfbcd76a1e5c3c53477978
root <med.cheffi@gmail.com>
2021-02-08 12:42:56 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const expressValidator = require('express-validator');
+const bodyParser = require('body-parser');
+const cors = require('cors'); 
+const bcryptjs = require('bcryptjs');
+const uuid = require('uuidv4');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+
+const app = express();
+
+dotenv.config();
+
+app.use(bodyParser.json());
+//app.use(cors);
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  
+    next();
+});
+
+app.get('/',(req,res) => {
+     console.log('GET HELLO WORLD')
+    res.send('Hello World')
+});
+
+mongoose
+.connect(
+  'mongodb+srv://' + process.env.DB_USER 
+                   +':' 
+                   + process.env.DB_PASSWORD 
+                   + '@cluster0.ubtin.mongodb.net/'
+                   + process.env.DB_NAME 
+                   + '?retryWrites=true&w=majority',{useNewUrlParser: true,useUnifiedTopology : true}
+)
+.then(() => {
+  console.log('connexion à la base mongoDB Cabinet Medical')
+  app.listen(5000, () => {
+    console.log(`Example app listening at http://localhost:5000`)
+  })
+   
+})
+.catch(err => {
+  console.log(err);
+});
+
